daa10ade3ae21b0e758adb17ef3a0b9bfd828ccc
Maialinda3007 <108538836+Maialinda3007@users.noreply.github.com>
2024-10-15 16:19:33 -0300

    s

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -22,7 +22,6 @@ async function quantidadeUsuarios() {
       plot_bgcolor: getCSS('--bg-color')
       paper_bgcolor: getCSS('--bg-color')
   }
-
   const grafico = document.createElement('div')
   grafico.className = 'grafico'
   document.getElementById('graficos-container').appendChild(grafico)
