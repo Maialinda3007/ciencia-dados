a7893583dbe727c01a11ef8ab59d07deff3eeb56
Maialinda3007 <108538836+Maialinda3007@users.noreply.github.com>
2024-10-15 15:51:39 -0300

    s

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,7 +1,8 @@
 async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
+    console.log(dados)
     const nomeDasRedes = Object.keys(dados)
     const quantidadeUsuarios = Object.values(dados)
   
