99c42034242776701702f2a73f015f65ba6ff187
Maialinda3007 <108538836+Maialinda3007@users.noreply.github.com>
2024-10-15 16:26:33 -0300

    s

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,4 +1,4 @@
-import { getCSS } from "./common.js"
+import { getCSS } from "./common.js";
 
 async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
