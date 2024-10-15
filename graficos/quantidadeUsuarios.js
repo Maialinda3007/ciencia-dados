203296a7d1394f61132f9b320d67687623cf964b
Maialinda3007 <108538836+Maialinda3007@users.noreply.github.com>
2024-10-15 18:26:05 -0300

    s

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -20,8 +20,18 @@ async function quantidadeUsuarios() {
 
     const layout = {
       plot_bgcolor: getCSS('--bg-color'),
-      paper_bgcolor:getCSS('--bg-color')
-  }
+      paper_bgcolor:getCSS('--bg-color'),
+      title: {
+        text: 'Redes sociais com mais usuários no mundo',
+         x: 0,
+         font: {
+             color: getCSS('--primary-color'),
+             family: getCSS('--font'),
+             size: 30
+         }
+      }
+    }
+  
     
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
