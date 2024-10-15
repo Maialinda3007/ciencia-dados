ef141b35cc6875931c4bb6e75c1a609754203a9c
Maialinda3007 <108538836+Maialinda3007@users.noreply.github.com>
2024-10-15 17:18:35 -0300

    s

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -50,12 +50,7 @@ async function quantidadeUsuarios() {
     }
     
 
-  const grafico = document.createElement('div')
-  grafico.className = 'grafico'
-  document.getElementById('graficos-container').appendChild(grafico)
-  Plotly.newPlot(grafico, data, layout)
-  
-  }
+ 
   
   quantidadeUsuarios()
     
