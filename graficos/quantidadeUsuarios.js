b20c4585aef7e060c3eedfb24450dfeff79f2f0d
Maialinda3007 <108538836+Maialinda3007@users.noreply.github.com>
2024-10-15 16:53:30 -0300

    s

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -30,28 +30,9 @@ async function quantidadeUsuarios() {
          size: 30
      }
       },
-      xaxis: {
-        tickfont{
-            color: getCSS('-primary-color'),
-            size: 16,
-            family:getCSS('--font')
-        },
-        title: {
-            text: 'nome das redes sociais',
-            font: {
-                color: getCSS('--secondary-color')
-            }
-        }
-    },
-    yaxis: {
-            text: 'bilhões de usuários ativos',
-            font: {
-                color: getCSS('--secondary-color')
-            }
-        }
-    }
-}
-  
+      
+      
+  }
   
     
 
